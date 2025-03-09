3a1db615a9bd9810624ac4f5156edacd2cc02216
meydodev <meydodev@gmail.com>
2025-03-09 20:57:06 +0100

    second commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import Routes from './src/app/navigation/Routes';
+import { StatusBar } from "expo-status-bar";
+import Routes from "./src/app/navigation/Routes";
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar translucent backgroundColor="transparent" />
+      <Routes />
+    </>
+  );
 }
-
-
