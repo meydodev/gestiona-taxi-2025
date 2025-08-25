1c6a4e382f079fd05428167913bcda5d0f617a69
meydodev <meydodev@gmail.com>
2025-08-25 10:44:30 +0200

    first commit

--- src/app/database/database-connection.tsx
+++
@@ -1,6 +0,0 @@
-import * as SQLite from 'expo-sqlite';
-
-// Create a connection to the database
-export const DatabaseConnection = {
-  getConnection: () => SQLite.openDatabaseAsync("gestiona_taxi_2025.db"),
-};
